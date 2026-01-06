8bfd76e213a4d316b6847aaf40b1090ba781433a
PipatpongS <156314552+PipatpongS@users.noreply.github.com>
2026-01-06 12:50:42 +0700

    Add files via upload

--- binary_tool.py
+++ binary_tool.py
@@ -1,30 +1,16 @@
 def text_to_binary(text: str) -> list:
     binary = []
-    for i in text:
-        ascii = ord(i)
-        char_binary = ""
-        while True:
-            rem = ascii % 2
-            char_binary += str(rem)
-            divisible = ascii // 2
-            if divisible == 0: break
-            ascii = divisible
-        while len(char_binary) < 8:
-            char_binary += str(0)
-        binary.append(char_binary[::-1])
+    for char in text:
+        binary.append(bin(ord(char))[2:])
     return binary
 
-def show_bianry(binary: list) -> None:
-    for i in binary:
-        print(i, end = " ")
-
-def binary_to_text(binary: list) -> str:
+def binary_to_text(binary_list: list) -> str:
     text = ""
-    for i in binary:
-        decimal = 0
-        binary_inv = i[::-1]
-        for j in range(len(i)):
-            tmp = int(binary_inv[j]) * (2**j)
-            decimal += tmp
-        text += chr(decimal)
+    for b in binary_list:
+        try:
+            if b.strip():
+                decimal = int(b, 2)
+                text += chr(decimal)
+        except:
+            text += "?"
     return text
